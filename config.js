d015babcc5f4f8af49b98acf0ca7cd9acac9ccdc
Jaxon Repp <jaxon@deliciousmonster.com>
2017-10-23 14:47:56 -0600

    Update packages

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 export default {
-  PK_HOST: 'pk.car.demo.niolabs.com',
+  PK_HOST: 'pk.demo.niolabs.com',
   PK_PORT: 443,
   PK_SECURE: true,
-  PK_JWT: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJwdWJrZWVwZXIiLCJzdWIiOiJ0ZXN0IiwiaXNzIjoicHVia2VlcGVyIiwicGVybSI6WyJhbGwiXX0.kMD7OZF_z6uqO16h0dbvVOJhzpsLPpDqMChfspq_i6c',
+  PK_JWT: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwZXJtIjpbImFsbCJdLCJzdWIiOiJkZW1vIiwiaXNzIjoicHVia2VlcGVyIiwiYXVkIjoicHVia2VlcGVyIn0.8Py05eBVPj6H40FxT85XR8rr-3XV7EmZmdqUTrjwF5U',
   WS_HOST: 'ws.demo.niolabs.com',
   WS_PORT: 443,
   WS_SECURE: true,
